27794d4911e0d9b9c651e70cd1400c5586907cd7
AgeevMaksim <amg.new2025@gmail.com>
2025-03-15 11:04:13 +0300

    добавил копию текстового вывода в стандартный поток stdout

--- 2025.03.09/1.py
+++ 2025.03.09/1.py
@@ -10,3 +10,10 @@ current_year = datetime.now().year
 #print (name + " " + name2 + ", " + str(current_year-int(birth)))
 #sep='' уберает все пробелы.  Позволяет легко изменять формат вывода, не изменяя сами данные.
 print (name,' ',name2,',',str(current_year-int(birth)),sep='')
+
+#C:\Users\admin>cd "C:\DS_Ageev\Repositories\Ageev\2025.03.09"
+#C:\DS_Ageev\Repositories\Ageev\2025.03.09>python 1.py
+#Введите имя:Максим
+#Введите фамилию:Агеев
+#Введите год рождения:1979
+#Максим Агеев,46
